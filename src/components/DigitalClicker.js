ec80f0ec7da19d496d8bf1ef323c293614fbf133
ferryrules <ferris@ferryrules.com>
2019-06-05 09:13:03 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+
+  state = {
+    timesClicked: 0
+  }
+
+  clicker = this.state.timesClicked
+
+  handleClick = (e) => {
+    this.setState({
+      timesClicked: this.clicker+=1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.clicker}</button>
+      </div>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,50 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: '1080p'
+      }
+    }
+  }
+
+  changeBitrate = (e) => {
+    this.setState({
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 12,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    })
+  }
+  changeResolution = (e) => {
+    this.setState({
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <p>I have {this.state.hasBeenClicked ? null : 'not'} been clicked!</p>
+        <button className="bitrate" onClick={this.changeBitrate}>bitrate</button>
+        <button className="resolution" onClick={this.changeResolution}>resolution</button>
+      </div>
+    )
+  }
+}
